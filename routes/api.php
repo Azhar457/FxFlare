5a173a7bbedadd29eb4cc1a75d63bb3242b57174
denss12 <dennis.233040167@mail.umpas.ac.id>
2025-12-29 04:46:45 +0700

    feat(api): tags api resources

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Dashboard\PostController;
 use App\Http\Controllers\Api\Dashboard\CategoryController;
+use App\Http\Controllers\Api\Dashboard\TagController;
 
 Route::middleware(['web', 'auth', 'admin'])
     ->prefix('dashboard')
@@ -16,6 +17,9 @@ Route::middleware(['web', 'auth', 'admin'])->prefix('dashboard')->group(function
     Route::get('/categories', [CategoryController::class, 'index']);
 });
 
+Route::middleware(['web', 'auth', 'admin'])->prefix('dashboard')->group(function () {
+    Route::get('/tags', [TagController::class, 'index']);
+});
 
 Route::middleware('web')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
